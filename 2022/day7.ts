b92300d8149f06d7100e4c73f347a70c0169da89
Andrew Walpole <walpolea@gmail.com>
2022-12-07 08:05:05 -0800

    Day 7

---
+++ 2022/day7.ts
@@ -0,0 +1,188 @@
+//deno run --allow-read day7.ts
+
+const fileData = await Deno.readTextFile("./day7.txt");
+
+const output = fileData.split('\n');
+
+type Command = {
+  keyword:string,
+  arg:String
+}
+
+interface IFile {
+  name:string;
+  size:number;
+}
+
+class DataFile implements IFile {
+  
+  constructor( public name:string, public size:number ) {
+
+  }
+
+}
+
+class Directory implements IFile {
+
+  public files:IFile[];
+
+  constructor( public name:string, public parent?:Directory ) {
+    this.files = new Array<IFile>();
+  }
+
+  addFile( file:IFile ) {
+    this.files.push( file );
+  }
+
+  get size():number {
+
+    let total = 0;
+    this.files.forEach( f => {
+      total += f.size;
+    });
+
+    return total;
+  }
+
+  get dirList():Directory[] {
+    return this.files.filter( f => f instanceof Directory ) as Directory[];
+  }
+
+  get dirMap():object {
+    return Object.fromEntries( this.dirList.map( d => [d.name, d]) );
+  }
+
+  getDir(name:string):Directory|undefined {
+    
+    if( name === ".." ) {
+      return this.parent;
+    }
+
+    return this.dirMap[name];
+  }
+}
+
+const COMMANDS = {
+  "cd": (dir, pwd?:Directory):Directory|undefined => {
+    if(!pwd) {
+      return new Directory(dir);
+    } else {
+      return pwd.getDir(dir);
+    }
+  },
+  "ls": (pwd?:Directory) => {
+    if( pwd ) {
+
+    }
+  }
+}
+
+function changeDirectory(dir, pwd?:Directory):Directory|undefined {
+  if(!pwd) {
+    return new Directory(dir);
+  } else {
+    return pwd.getDir(dir);
+  }
+}
+
+function processOutput( output:string[], pwd?:Directory ) {
+
+  let currentLine:string[] | undefined = output.shift()?.split(' ');
+
+  if( currentLine?.[0] === "$" ) {
+
+    const [_, cmd, arg] = currentLine;
+    switch( cmd ) {
+      case "cd":
+        pwd = changeDirectory(arg, pwd);
+        break;
+      case "ls":
+        break;
+    }
+
+  } else if(currentLine?.length === 2 ) {
+    
+    let f:IFile;
+
+    if( currentLine?.[0] === "dir") {
+      f = new Directory( currentLine?.[1], pwd );
+    } else {
+      f = new DataFile( currentLine[1], parseInt( currentLine[0]) );
+    }
+
+    pwd?.addFile(f);
+
+  }
+
+  while( output.length ) {
+    processOutput( output, pwd );
+  }
+
+  return pwd;
+}
+
+
+
+function getUndersizedDirectories( dir:Directory, sizeLimit:number ):Directory[] {
+
+  let underLimit = new Array<Directory>();
+
+  if( dir.size <= sizeLimit) {
+    underLimit.push(dir);
+  }
+
+  if( dir.dirList.length ) {
+    dir.dirList.forEach( d => {
+      underLimit = [...getUndersizedDirectories(d, sizeLimit), ...underLimit];
+    })
+  }
+
+  return underLimit;
+}
+
+function getOversizedDirectories( dir:Directory, sizeLimit:number ):Directory[] {
+
+  let underLimit = new Array<Directory>();
+
+  if( dir.size >= sizeLimit) {
+    underLimit.push(dir);
+  }
+
+  if( dir.dirList.length ) {
+    dir.dirList.forEach( d => {
+      underLimit = [...getOversizedDirectories(d, sizeLimit), ...underLimit];
+    })
+  }
+
+  return underLimit;
+}
+
+//Part 1
+const rootDir = processOutput( output );
+
+
+if( rootDir ) {
+  
+  const undersized = getUndersizedDirectories(rootDir, 100000).map( d => {return { name: d.name, size: d.size }} );
+  
+  console.log( undersized );
+
+  const total = undersized.reduce( (sum, dir) => sum + dir.size, 0);
+
+  console.log("PART 1:", total);
+
+  //Part 2
+
+  const TOTAL_SPACE = 70_000_000;
+  const NEEDED_SPACE = 30_000_000;
+
+  const unusedSpace = TOTAL_SPACE - rootDir?.size;
+  const spaceNeeded = NEEDED_SPACE - unusedSpace;
+
+  console.log( unusedSpace, spaceNeeded);
+
+  const overSizedDirs = getOversizedDirectories(rootDir, spaceNeeded ).sort( (a,b) => a.size - b.size ).map( d => {return { name: d.name, size: d.size }} ) ;
+
+  console.log( "PART 2:", overSizedDirs[0] );
+
+}
